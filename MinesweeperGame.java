08d1970fe824caea2809e4af09f3b85c72ed1c5d
VCordovaMSU <111928526+VCordovaMSU@users.noreply.github.com>
2022-10-25 16:53:50 -0600

    Update MinesweeperGame.java

--- MinesweeperGame.java
+++ MinesweeperGame.java
@@ -1,7 +1,7 @@
 /*
  * CS2050 - Computer Science II - Fall 2022
  * Instructor: Thyago Mota
- * Student(s) Name(s):
+ * Student(s) Name(s): Vincent Cordova, Ubaldo Bogarin
  * Description: prg_02 - Minesweeper
  */
 
@@ -21,13 +21,17 @@ public class MinesweeperGame {
         int size;
 
         Scanner s = new Scanner(System.in);
-        System.out.println("Choose your difficulty: 1, 2, 3");
+        System.out.println("Choose your difficulty number: ");
+        System.out.println("BEGINNER (1)");
+        System.out.println("INTERMEDIATE (2)");
+        System.out.println("EXPERT (3)");
+        System.out.print("Difficulty (1, 2 or 3): ");
         int diff = s.nextInt();
 
-        if (diff == 3) {
+        if (diff == EXPERT_LEVEL) {
             size = 20;
             mines = 50;
-        } else if (diff == 2) {
+        } else if (diff == INTERMEDIATE_LEVEL) {
             size = 15;
             mines = 25;
         } else {
@@ -37,14 +41,34 @@ public class MinesweeperGame {
 
         Minesweeper minesweeper = new Minesweeper(size, mines);
 
-        while (!minesweeper.isGameOver()) {
-            System.out.print("Pick a coordinate to check (x) (y) : ");
-            int x = s.nextInt();
-            int y = s.nextInt();
+        int row = 0;
+        int column = 0;
 
-            minesweeper.reveal(x, y);
+
+        while (!minesweeper.isGameOver()) {
+            boolean flag = false;
+            System.out.println("Choose a row and column in range 0 - " + (size - 1));
+            System.out.print("Row, Column: ");
+
+            String x = s.next();
+
+
+            if (x.charAt(0) == '+') {
+                flag = true;
+                row = Integer.parseInt(x.substring(1).split(",")[0]);
+                column = Integer.parseInt(x.substring(1).split(",")[1]);
+                System.out.println(row);
+                System.out.println(column);
+            }
+            else {
+                row = Integer.parseInt(x.split(",")[0]);
+                column = Integer.parseInt(x.split(",")[1]);
+            }
+
+            System.out.println();
+            if(flag) minesweeper.reveal(row, column, true);
+            else minesweeper.reveal(row, column);
             System.out.println(minesweeper);
         }
-
     }
 }
